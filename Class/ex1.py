b29aa7c806f6021a8988e83bb9f674522a41380d
jayhebe <jayhebe1983@sina.com>
2020-01-30 23:05:06 +0800

    python exercises 20200130

---
+++ Class/ex1.py
@@ -0,0 +1,31 @@
+"""
+Write a Python class to convert an integer to a roman numeral.
+"""
+
+
+class IntToRoman:
+    def int_to_roman(self, num):
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+        ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+        roman_num = ''
+        i = 0
+        while num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+
+
+print(IntToRoman().int_to_roman(1))
+print(IntToRoman().int_to_roman(4000))
